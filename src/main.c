d085c863bf7e31855d4d0dead01a402f294e0125
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2024-11-12 15:20:09 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,49 +1,33 @@
-#include <stdio.h>
+#include <stdio.h> 
 #include <stdlib.h>
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]) {
-    if (argc == 4 || (argc == 3 && strcmp(argv[1], "car") == 0)) {
-        char* op = argv[1];
-        double r = 0;
-
-        // Cas où l'opération est "add", "sub", "mul", ou "div" (nécessite 3 arguments)
-        if (argc == 4) {
-            double a = atof(argv[2]);
-            double b = atof(argv[3]);
-
-            if (strcmp(op, "add") == 0) {
-                r = _add(a, b);
-                printf("%lf\n", r);
-            } else if (strcmp(op, "sub") == 0) {
-                r = _sub(a, b);
-                printf("%lf\n", r);
-            } else if (strcmp(op, "mul") == 0) {
-                r = _mul(a, b);
-                printf("%lf\n", r);
-            } else if (strcmp(op, "div") == 0) {
-                if (b != 0) {
-                    r = _div(a, b);
-                    printf("%lf\n", r);
-                } else {
-                    printf("Erreur : Division par zéro\n");
-                }
-            } else {
-                printf("Erreur : Opération inconnue\n");
-            }
-        }
-        // Cas où l'opération est "car" (nécessite 2 arguments)
-        else if (argc == 3 && strcmp(op, "car") == 0) {
-            double a = atof(argv[2]);
-            r = _car(a);
-            printf("%lf\n", r);
-        } else {
-            printf("Erreur : Opération ou nombre d'arguments incorrect\n");
-        }
-    } else {
-        printf("Erreur : Nombre d'arguments incorrect\n");
-    }
-    return 0;
+int main(int argc, char *argv[]){
+	if (argc == 4)
+	{
+	char* op = argv[1];
+	char* a = argv[2];
+	char* b = argv[3];
+	double r = 0;
+	if (strcmp(op, "add") == 0) {
+		r = _add(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "sub") == 0) {
+		r = _sub(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "mul") == 0) {
+		r = _mul(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else if (strcmp(op, "div") == 0) {
+		r = _div(atof(a), atof(b));
+		printf("%lf",r);
+	}
+	else {printf("Erreur de parametres");}
+	}
+	else {printf("Erreur de parametres");}
+	return 0;
 }
-
