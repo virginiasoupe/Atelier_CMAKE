ccaba30098e756ebf5731acb1354f82cb51dce38
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2024-11-12 15:12:19 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,34 +1,38 @@
-int main(int argc, char argv[]) {
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "./../include/calculator.h"
+
+int main(int argc, char *argv[]) {
     if (argc == 4 || (argc == 3 && strcmp(argv[1], "car") == 0)) {
-        char op = argv[1];
+        char* op = argv[1];
         double r = 0;
 
-        if (strcmp(op, "add") == 0) {
+        if (strcmp(op, "add") == 0 && argc == 4) {
             r = _add(atof(argv[2]), atof(argv[3]));
-            printf("%lf", r);
+            printf("%lf\n", r);
         }
-        else if (strcmp(op, "sub") == 0) {
+        else if (strcmp(op, "sub") == 0 && argc == 4) {
             r = _sub(atof(argv[2]), atof(argv[3]));
-            printf("%lf", r);
+            printf("%lf\n", r);
         }
-        else if (strcmp(op, "mul") == 0) {
+        else if (strcmp(op, "mul") == 0 && argc == 4) {
             r = _mul(atof(argv[2]), atof(argv[3]));
-            printf("%lf", r);
+            printf("%lf\n", r);
         }
-        else if (strcmp(op, "div") == 0) {
+        else if (strcmp(op, "div") == 0 && argc == 4) {
             r = _div(atof(argv[2]), atof(argv[3]));
-            printf("%lf", r);
+            printf("%lf\n", r);
         }
         else if (strcmp(op, "car") == 0 && argc == 3) {
             r = _car(atof(argv[2]));
-            printf("%lf", r);
+            printf("%lf\n", r);
         }
         else {
-            printf("Erreur de paramètres");
+            printf("Erreur de paramètres\n");
         }
-    }
-    else {
-        printf("Erreur de paramètres");
+    } else {
+        printf("Erreur de paramètres\n");
     }
     return 0;
 }
