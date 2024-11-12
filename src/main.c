17ad8646d69750a60ce4b8bb5cee96e3ed3ce652
virginiasoupe <159542972+virginiasoupe@users.noreply.github.com>
2024-11-12 14:50:37 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,31 +3,41 @@
 #include <string.h>
 #include "./../include/calculator.h"
 
-int main(int argc, char *argv[]){
-	if (argc == 4)
-	{
-	char* op = argv[1];
-	char* a = argv[2];
-	char* b = argv[3];
-	double r = 0;
-	if (strcmp(op, "add") == 0) {
-		r = _add(atof(a), atof(b));
-		printf("%lf",r);
+int main(int argc, char *argv[]) {
+	if (argc == 4 || (argc == 3 && strcmp(argv[1], "car") == 0)) {
+		char* op = argv[1];
+		char* a = argv[2];
+		double r = 0;
+
+		if (strcmp(op, "add") == 0) {
+			r = _add(atof(a), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "sub") == 0) {
+			r = _sub(atof(a), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "mul") == 0) {
+			r = _mul(atof(a), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "div") == 0) {
+			r = _div(atof(a), atof(argv[3]));
+			printf("%lf", r);
+		}
+		else if (strcmp(op, "car") == 0) {
+			r = _car(atof(a));
+			printf("%lf", r);
+		}
+		else {
+			printf("Erreur de parametres");
+		}
+	} else {
+		printf("Erreur de parametres");
 	}
-	else if (strcmp(op, "sub") == 0) {
-		r = _sub(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "mul") == 0) {
-		r = _mul(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else if (strcmp(op, "div") == 0) {
-		r = _div(atof(a), atof(b));
-		printf("%lf",r);
-	}
-	else {printf("Erreur de parametres");}
-	}
+	return 0;
+}
+
 	else {printf("Erreur de parametres");}
 	return 0;
 }
